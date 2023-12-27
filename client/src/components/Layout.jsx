601152b800c610241734236e6c14309541a5a0f6
ghost-sys-coder <juniorbeast177@gmail.com>
2023-12-27 14:03:16 +0300

    Fix unresponsive screen

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -26,9 +26,9 @@ const Layout = () => {
   return (
     <>
       {isAuthenticated ? (
-        <main className="max-w-[1440px] mx-auto min-h-screen flex">
+        <main className="min-h-screen flex">
           <Sidebar />
-          <div className="flex-1 flex flex-col overflow-hidden">
+          <div className="flex-1 flex flex-col overflow-hidden max-w-[1440px] mx-auto">
             <Navbar />
             <Outlet />
           </div>
